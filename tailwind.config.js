a1cd89e8a959068766023356fdf6bc5b1a29610c
XibalbaM <maelporret@outlook.fr>
2024-01-24 21:37:46 +0100

    chore: setup all build tools

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./**/*.{html,js}"],
+    theme: {
+        fontFamily: {
+            titles: ["'Montserrat', sans-serif"],
+            paragraphs: ["'Playfair Display', sans-serif"],
+        },
+        extend: {},
+    },
+    plugins: [],
+}
+
